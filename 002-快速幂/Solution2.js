28bf1a08629899674ab1340bf8a27084296efc0d
zhuxianzhong <22525482@qq.com>
2022-10-05 00:44:58 +0800

    002-快速幂

---
+++ 002-快速幂/Solution2.js
@@ -0,0 +1,19 @@
+// 快速幂迭代写法
+var myPow = function (x, n) {
+  return n >= 0 ? quickPow(x, n) : 1 / quickPow(x, -n);
+};
+
+function quickPow(x, n) {
+  let res = 1;
+  let t = x;
+
+  while (n > 0) {
+    if (n & 1 != 0) {
+      res *= t;
+    }
+    t *= t;
+
+    n = Math.floor(n / 2);
+  }
+  return res;
+}
