e8ca30044e26b919ca061e9d045bf9035f65d675
zhuxianzhong <22525482@qq.com>
2023-02-26 23:45:36 +0800

    01-图的深度优先遍历

---
+++ 018-图论/01-图的深度优先遍历/01-深度优先遍历.js
@@ -0,0 +1,30 @@
+function dfs(edges, n) {
+  let graph = Array(n)
+  for (let i = 0; i < n; i++) {
+    graph[i] = new Set()
+  }
+
+  for (let i = 0; i < edges.length; i++) {
+    let v1 = edges[i][0]
+    let v2 = edges[i][1]
+    graph[v1].add(v2)
+    graph[v2].add(v1)
+  }
+
+  let visited = Array(n).fill(false)
+  let res = []
+  
+  function __dfs(v) {
+    visited[v] = true
+
+    console.log('遍历v节点')
+    res.push(v)
+    for (let w of graph[v]) {
+      if (!visited[w]) {
+        __dfs(w)
+      }
+    }
+  }
+
+  return res
+}
